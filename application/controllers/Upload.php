b977f58ec093013c629b8b022a8e7b3b857f3604
Nakul Rathore <nakul.rathore@learner.manipal.edu>
2019-05-17 12:55:23 +0530

    ggwpqwer

--- application/controllers/Upload.php
+++ application/controllers/Upload.php
@@ -104,8 +104,13 @@ class Upload extends CI_Controller
                                         //         $e->getMessage();
                                         // }
                                         // $ggwpez=file_get_contents(`
-                                        $urlstr="https://api.elasticemail.com/v2/email/send?apikey=dfd01946-4450-45a3-b414-d13087b56302&&subject=PHP%20TASK%20INVITE&to=".$arr[1]."&from=nakulrathore97@gmail.com&fromName=Nakul%20Rathore&bodyText=Dear%20".$arr[0]."%2C%0AYou%20are%20invited%20to%20this%20sample%20website.%0AHere%20are%20your%20credentials%3A%0Ausername%3A%20".$arr[1]."%0Apassword%3A%20password%0A%0ASincerely%2C%0APHP%20Task%0A&trackClicks=false&trackOpens=false&msgFromName=Nakul%20Rathore";
-                                        $ggwpez=file_get_contents($urlstr);
+                                        set_error_handler(function(){
+                                                
+                                                //to handle file_get_contents errorrs
+                                        });
+                                        $urlstr = "https://api.elasticemail.com/v2/email/send?apikey=dfd01946-4450-45a3-b414-d13087b56302&&subject=PHP%20TASK%20INVITE&to=" . $arr[1] . "&from=nakulrathore97@gmail.com&fromName=Nakul%20Rathore&bodyText=Dear%20" . $arr[0] . "%2C%0AYou%20are%20invited%20to%20this%20sample%20website.%0AHere%20are%20your%20credentials%3A%0Ausername%3A%20" . $arr[1] . "%0Apassword%3A%20password%0A%0ASincerely%2C%0APHP%20Task%0A&trackClicks=false&trackOpens=false&msgFromName=Nakul%20Rathore";
+                                        $ggwpez = file_get_contents($urlstr);
+                                        restore_error_handler();
                                         $this->db->set('created_on', 'NOW()', FALSE);
                                         $flag = $this->Insert_model->form_insert($fdata);
                                         $num = $num + 1;
