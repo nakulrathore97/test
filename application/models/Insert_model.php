3d579b95a3d86e07b596e2ff10735d6643b8df0c
Nakul Rathore <nakul.rathore@learner.manipal.edu>
2019-05-17 18:06:43 +0530

    final

--- application/models/Insert_model.php
+++ application/models/Insert_model.php
@@ -14,14 +14,3 @@ class Insert_model extends CI_Model
 }
 ?>
 
-<!-- SQL CREATE statement --$this->load->model('MY_MODEL');>
- use HS; // for using database HS-->
-<!-- CREATE TABLE my_users(
-    user_id int(10) NOT NULL AUTO_INCREMENT PRIMARY KEY, -->
-    <!-- full_name varchar(255) NOT NULL,
-    email varchar(255) NOT NULL,
-    password varchar(255) NOT NULL,
-    created_by int,
-    is_admin BOOLEAN,
-    created_on TIMESTAMP
-) -->
