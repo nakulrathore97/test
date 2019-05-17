56271d555e61373a69480a70da5ff0deadfa9ab9
Nakul Rathore <nakul.rathore@learner.manipal.edu>
2019-05-17 12:15:33 +0530

    ggwp

---
+++ application/logs/log-2019-05-17.php
@@ -0,0 +1,10 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-05-17 11:11:04 --> Severity: error --> Exception: Class 'ElasticEmailClient\ApiClient' not found /var/www/html/codeigniter/application/controllers/Upload.php 29
+ERROR - 2019-05-17 11:14:56 --> Severity: 8192 --> Non-static method ElasticEmailClient\ApiConfiguration::setApiKey() should not be called statically /var/www/html/codeigniter/application/controllers/Upload.php 29
+ERROR - 2019-05-17 11:14:56 --> Severity: error --> Exception: Using $this when not in object context /var/www/html/codeigniter/application/controllers/ElasticEmailClient.php 69
+ERROR - 2019-05-17 11:16:57 --> Severity: error --> Exception: Class 'ElasticEmailClient\Email' not found /var/www/html/codeigniter/application/controllers/Upload.php 44
+ERROR - 2019-05-17 11:25:31 --> Severity: error --> Exception: Class 'GuzzleHttp\Client' not found /var/www/html/codeigniter/application/controllers/ElasticEmailClient.php 192
+ERROR - 2019-05-17 11:40:17 --> Severity: error --> Exception: Class 'GuzzleHttp\Client' not found /var/www/html/codeigniter/application/controllers/ElasticEmailClient.php 192
+ERROR - 2019-05-17 11:40:49 --> Severity: error --> Exception: Class 'GuzzleHttp\Client' not found /var/www/html/codeigniter/application/controllers/ElasticEmailClient.php 192
+ERROR - 2019-05-17 11:42:13 --> Severity: error --> Exception: Class 'ElasticEmailClient\ApiConfiguration' not found /var/www/html/codeigniter/application/controllers/Upload.php 45
